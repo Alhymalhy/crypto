3afe23338cf1a57c4679d44459e571e7f555f9cf
triangle <sad.station@outlook.com>
2023-12-09 00:36:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -45,6 +45,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/crypto/',
   plugins: [
     vue(),
     AutoImport({
